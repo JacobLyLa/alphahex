662d17533c39831fd662f4ca3fb04ed19075c2c5
JacobLyLa <Jacob.LLarsen@hotmail.com>
2023-02-04 17:58:28 +0100

    added neuralnet players

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import random
 from mcts import Mcts
+import numpy as np
 
 # Player to be used in a game, only needs to implement the play method.
 class Player:
@@ -16,9 +17,9 @@ class HumanPlayer(Player):
     def playAction(self, game):
         print("Turn: " + str(game.getTurn()))
         print("Game State: " + str(game.getStringState()))
+        print("Legal Actions: ", end="")
         legalActions = game.getActions()
         # enumerate the legal actions
-        print("Legal Actions: ", end="")
         for i, action in enumerate(legalActions):
             print(f'{action}[{i+1}]', end=" ")
         print()
@@ -46,4 +47,39 @@ class MCTSPlayer(Player):
     def playAction(self, game):
         action = self.mcts.search(game)
         game.playAction(action)
-        # print("MCTS Action: " + str(action))
+
+class NeuralNetPlayer(Player):
+    def __init__(self, name="NeuralNet", model=None, argmax=True):
+        super().__init__(name)
+        self.model = model
+        self.argmax = argmax
+
+    def getAction(self, game):
+        actionProbs = self.model.predict(game.getNNState(), verbose=0)
+        # replace illegal actions with 0
+        legalActions = game.getActionsMask()
+        for i in range(len(actionProbs[0])):
+            if legalActions[i] == 0:
+                actionProbs[0][i] = 0
+        if self.argmax:
+            action = np.argmax(actionProbs)
+        else:
+            # normalize the action probabilities
+            actionProbs = actionProbs / np.sum(actionProbs)
+            action = np.random.choice(len(actionProbs[0]), p=actionProbs[0])
+        return action
+
+    def playAction(self, game):
+        action = self.getAction(game)
+        game.playAction(action)
+
+class NeuralMCTSPlayer(Player):
+    def __init__(self, name="NeuralMCTS", model=None, maxIters=100, maxTime=1):
+        super().__init__(name)
+        self.model = model
+        self.rolloutPolicy = lambda game: NeuralNetPlayer(name="RolloutPolicy", model=model, argmax=False).getAction(game)
+        self.mcts = Mcts(maxIters, maxTime, self.rolloutPolicy)
+
+    def playAction(self, game):
+        action = self.mcts.search(game)
+        game.playAction(action)
