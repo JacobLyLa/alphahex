eb3c913dcc9b1abb1b22640508c95ff8ab91fd00
JacobLyLa <Jacob.LLarsen@hotmail.com>
2023-02-04 15:18:39 +0100

    index based action input, also some cleaning

--- player.py
+++ player.py
@@ -1,26 +1,32 @@
 import random
-from MCTS import Mcts
+from mcts import Mcts
 
 # Player to be used in a game, only needs to implement the play method.
 class Player:
-    def __init__(self, name, verbose=False):
+    def __init__(self, name):
         self.name = name
-        self.verbose = verbose
 
     def playAction(self, game):
         raise NotImplementedError
 
 class HumanPlayer(Player):
-    def __init__(self, name="Human", verbose=True):
+    def __init__(self, name="Human"):
         super().__init__(name)
-        self.verbose = verbose
 
     def playAction(self, game):
-        if self.verbose:
-            print("Turn: " + str(game.getTurn()))
-            print("Game State: " + str(game.getState()))
-            print("Legal Actions: " + str(game.getActions()))
-        action = int(input("Enter action: "))
+        print("Turn: " + str(game.getTurn()))
+        print("Game State: " + str(game.getStringState()))
+        legalActions = game.getActions()
+        # enumerate the legal actions
+        print("Legal Actions: ", end="")
+        for i, action in enumerate(legalActions):
+            print(f'{action}[{i+1}]', end=" ")
+        print()
+        actionIndex = int(input("Enter action index: "))
+        if actionIndex < 1 or actionIndex > len(legalActions):
+            print("Invalid action index")
+            return self.playAction(game)
+        action = legalActions[actionIndex - 1]
         print()
         game.playAction(action)
 
@@ -33,12 +39,11 @@ class RandomPlayer(Player):
         game.playAction(action)
 
 class MCTSPlayer(Player):
-    def __init__(self, name="MCTS"):
+    def __init__(self, name="MCTS", maxIters=100, maxTime=1):
         super().__init__(name)
-        self.mcts = Mcts()
+        self.mcts = Mcts(maxIters, maxTime)
 
     def playAction(self, game):
-        action = self.mcts.search(game, self.verbose)
+        action = self.mcts.search(game)
         game.playAction(action)
-        if self.verbose:
-            print("MCTS Action: " + str(action))
+        # print("MCTS Action: " + str(action))
