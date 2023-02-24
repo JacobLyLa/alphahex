26c16f43633b1d1353eab2e43e35ca17f6c8a7c2
JacobLyLa <Jacob.LLarsen@hotmail.com>
2023-02-24 23:32:52 +0100

    shortened net and example training

--- neuralnet.py
+++ neuralnet.py
@@ -11,8 +11,28 @@ outputSize = size*size
 def getModel():
     model = Sequential()
     model.add(Dense(16, input_dim=inputSize, activation='relu'))
-    model.add(Dense(16, activation='relu'))
-    model.add(Dense(16, activation='relu'))
     model.add(Dense(outputSize, activation='softmax'))
-    model.compile(loss='mean_squared_error', optimizer=Adam(learning_rate=0.001))
+    model.compile(loss='mean_squared_error', optimizer=Adam(learning_rate=0.01))
     return model
+
+if __name__ == '__main__':
+    from hex import HexGame
+    from player import RandomPlayer, NeuralNetPlayer, NeuralMCTSPlayer, MCTSPlayer
+    from tournament import Tournament
+    rounds = 1
+    model = getModel()
+    nnMctsPlayer = NeuralMCTSPlayer(model=model, maxIters=30, maxTime=5)
+    randomPlayer = RandomPlayer()
+    tournament = Tournament(HexGame, nnMctsPlayer, randomPlayer)
+    tournament.run(rounds)
+    wins, losses, draws = tournament.getResults()
+    print(f"NN MCTS Player: {wins} wins, {losses} losses, {draws} draws")
+
+    replay = nnMctsPlayer.mcts.replayBuffer
+    print(replay)
+    print(f'Length of replay buffer: {len(replay)}')
+
+    # train model
+    X = np.array([x[0] for x in replay])
+    y = np.array([x[1] for x in replay])
+    model.fit(X, y, epochs=10, verbose=1)
