ab8ee9b595aee1e1287916959f4a56c961a1cf9f
dagokl <dag.b.okland@gmail.com>
2023-02-24 20:33:52 +0100

    plot hex board in matplotlib

--- hex.py
+++ hex.py
@@ -1,6 +1,9 @@
-import numpy as np
 import random
 import logging
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import RegularPolygon
+from math import sqrt
 from game import Game
 logging.basicConfig()
 # logging.getLogger().setLevel(logging.DEBUG)
@@ -9,13 +12,17 @@ logging.basicConfig()
 NEIGHBORS = [(0, 1), (1, 0), (1, -1), (0, -1), (-1, 0), (-1, 1)]
 
 class HexGame(Game):
-    def __init__(self, player1, player2, size=4):
+    def __init__(self, player1, player2, size=4, plot=False):
         self.player1 = player1
         self.player2 = player2
         self.size = size
         self.board = np.zeros(shape=(size,size), dtype=np.int8)
         self.turn = 1
         self.log = logging.getLogger(__name__)
+
+        self.plot = plot
+        if plot:
+            self.plotter = HexPlotter(self)
 
     def getStringState(self):
         return self.board.copy()
@@ -73,6 +80,8 @@ class HexGame(Game):
 
         self.log.debug(f'player {self.turn} made move {action} resulting in board state:\n{self.board}\n')
 
+        if self.plot:
+            self.plotter.plot_action(action)
 
     def isTerminal(self):
         return self.hasPlayerWon(1) or self.hasPlayerWon(-1)
@@ -137,11 +146,51 @@ class HexGame(Game):
 
         return False
 
+
+class HexPlotter():
+    def __init__(self, game: HexGame, R=0.5, pause_after_plot=0.001):
+        self.game = game
+        self.pause_after_plot = pause_after_plot
+
+        # distance between center and corner
+        self.dist_corner = R
+        # distance between center and middle of side
+        self.dist_side = R * sqrt(3) / 2
+
+        fig, ax = plt.subplots(1, figsize=(10, 8))
+        ax.axis('off')
+        ax.set_aspect('equal')
+        padding = 0.02
+        ax.set_xlim(-self.dist_side - padding, (3 * game.size - 2) * self.dist_side + padding)
+        ax.set_ylim(-(1.5 * game.size - 0.5) * self.dist_corner - padding, self.dist_corner + padding)
+        self.ax = ax
+
+        self.colors = {1: 'red', -1: 'blue', 0: 'white'}
+
+        self.plot_board()
+
+    def plot_board(self):
+        for x in range(self.game.size):
+            for y in range(self.game.size):
+                self.plot_hexagon(x, y, self.colors[self.game.board[x, y]])
+        plt.pause(self.pause_after_plot)
+
+    def plot_action(self, action):
+        self.plot_hexagon(action[0], action[1], self.colors[-1 * self.game.turn])
+        plt.pause(self.pause_after_plot)
+
+    def plot_hexagon(self, row, col, color='white'):
+        x = 2 * self.dist_side * col + self.dist_side * row
+        y = - 1.5 * self.dist_corner * row
+        hex = RegularPolygon((x, y), numVertices=6, radius=self.dist_corner, orientation=0, facecolor=color, edgecolor='black', alpha=0.8, linewidth=2)
+        self.ax.add_patch(hex)
+
+
 if __name__ == '__main__':
     from player import RandomPlayer
     from tournament import Tournament
     r1, r2 = RandomPlayer("Random1"), RandomPlayer("Random2")
-    tournament = Tournament(HexGame, r1, r2)
+    tournament = Tournament(HexGame, r1, r2, plot=True)
     tournament.run(10)
     wins, losses, draws = tournament.getResults()
     print(f'{r1.name} won {wins} times, {r2.name} won {losses} times, and there were {draws} draws')
--- tournament.py
+++ tournament.py
@@ -14,22 +14,24 @@ def savePlayer(player):
 
 # TODO: tournment between M agents
 class Tournament:
-    def __init__(self, game, player1, player2):
+    def __init__(self, game, player1, player2, boardSize=4, plot=False):
         self.game = game
+        self.boardSize = boardSize
         self.player1 = player1
         self.player2 = player2
         self.player1Wins = 0
         self.player2Wins = 0
         self.draws = 0
+        self.plot = plot
 
     def run(self, games):
         # random player starts
         swapSide = random.randint(0, 1)
         for _ in range(games):
             if swapSide == 1:
-                game = self.game(self.player2, self.player1)
+                game = self.game(self.player2, self.player1, self.boardSize, plot=self.plot)
             else:
-                game = self.game(self.player1, self.player2)
+                game = self.game(self.player1, self.player2, self.boardSize, plot=self.plot)
             game.playGame()
 
             result = game.getResult()
