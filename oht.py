08fb981cb9f5391bc5b0a82d1ced56a4729b1a2b
dagokl <dag.b.okland@gmail.com>
2023-04-23 22:57:36 +0200

    Online Hex Tournament with random player

---
+++ oht.py
@@ -0,0 +1,82 @@
+from math import sqrt
+import numpy as np
+
+from client import ActorClient
+from player import RandomPlayer
+from hex import HexGame
+
+
+class MyClient(ActorClient):
+    def __init__(self):
+        super().__init__(qualify=False)
+
+    def handle_series_start(self, unique_id, series_id, player_map, num_games, game_params):
+        super().handle_series_start(unique_id, series_id, player_map, num_games, game_params)
+        self.gameSize = game_params[0]
+
+    def handle_game_start(self, start_player):
+        super().handle_game_start(start_player)
+        self.game = HexGame(
+            player1=RandomPlayer(name="Random"),
+            player2=None,
+            size=self.gameSize
+        )
+        self.lastBoard = self.game.board.copy()
+
+        # In the OHT implementation player 1 always goes from top to bottom
+        # and player 2 always goes from left to right.
+        # This matches our implementation in hex.py
+        # However the OHT implementation of Hex both players may start
+        # this does not match out implementation of hex, so we need to
+        # swap the turn if player 2 starts.
+        if start_player == 2:
+            self.game.turn = -1
+
+    def handle_get_action(self, state):
+        assert state[0] == 1, 'Only support for local player playing as player 1'
+
+        board = stateToBoard(state)
+        opponentAction = actionFromBoardDiff(board, self.lastBoard)
+        if opponentAction is not None:
+            self.game.playAction(opponentAction)
+            self.lastBoard = self.game.board.copy()
+
+        self.game.player1.playAction(self.game)
+        action = actionFromBoardDiff(self.game.board, self.lastBoard)
+        self.lastBoard = self.game.board.copy()
+        return action
+
+    def handle_game_end(self, result):
+        super().handle_game_end(result)
+
+    def handle_series_end(self, results):
+        super().handle_series_end(results)
+
+    def handle_tournament_over(self, score):
+        super().handle_tournament_over(score)
+
+
+def actionFromBoardDiff(board, lastBoard):
+    diff = board - lastBoard
+
+    if np.sum(diff) == 0:
+        return None
+
+    action = np.where(diff != 0)
+    return (int(action[0][0]), int(action[1][0]))
+
+
+def stateToBoard(state):
+    size = int(sqrt(len(state) - 1))
+    board = np.array(state[1:]).reshape(size, size)
+    board[board == 2] = -1
+    return board
+
+
+def main():
+    client = MyClient()
+    client.run()
+
+
+if __name__ == "__main__":
+    main()
